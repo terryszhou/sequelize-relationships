07e46637ded408ebed972c496a68754c3ce0966d
weston-bailey <wdbailey87@gmail.com>
2021-06-03 12:01:59 -0700

    pre lunch

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 const db = require('./models')
 
 // CREATE
-db.user.create({
-  firstName: 'Anna',
-  lastName: 'Zocher',
-  age: 30,
-  email: "azocher@ga.co"
-}).then(createdUser=>{
-  console.log(createdUser)
-})
+// db.user.create({
+//   firstName: 'Anna',
+//   lastName: 'Zocher',
+//   age: 30,
+//   email: "azocher@ga.co"
+// }).then( createdUser => {
+//   console.log(createdUser)
+// })
+
+async function createUser() {
+  try {
+    const createdUser = await db.user.create({
+      firstName: 'Nick',
+      lastName: 'Schmitt',
+      age: 29,
+      email: 'birthdayboy@gmail.com'
+    })
+
+    console.log(createdUser)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// createUser()
 
 // READ
-db.user.findOne({
-  where: { firstName: 'Anna' }
-}).then( foundUser=> {
-  console.log(foundUser)
-})
+// db.user.findOne({
+//   where: { firstName: 'Nick' }
+// }).then( foundUser=> {
+//   console.log(foundUser)
+// })
+
+
+// refactor finding nick in the DB to async/await
+async function findNick() {
+  try {
+    const foundUser = await db.user.findOne({
+      where: { firstName: 'Nick' }
+    })
+
+    console.log(foundUser)
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+findNick()
 
 // UPDATE
 
